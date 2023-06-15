b2233cc52468db05c960eb8b92e9dfac12303a53
Sidney Shafer <sidney@mnshafers.com>
2023-06-15 07:02:45 -0500

    Created Header Card Button

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -1,13 +1,14 @@
 import { Fragment } from 'react';
 import mealsImage from '../../assets/meals.jpg';
 import classes from './Header.module.css';
+import HeaderCardButton from './HeaderCardButton';
 
 const Header = (props) => {
   return (
     <Fragment>
       <header className={classes.header}>
         <h1>Meals</h1>
-        <button>Cart</button>
+        <HeaderCardButton />
       </header>
       <div className={classes['main-image']}>
         <img src={mealsImage} alt='A table full of delicious food.' />
